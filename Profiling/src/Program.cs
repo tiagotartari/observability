39e2793561f2fda05ecdd46f663bbd77ab9e8c13
Tiago Tartari <tiagotartari@hotmail.com>
2023-05-19 11:06:27 -0300

    adding pyroscope with .NET 7

---
+++ Profiling/src/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+Pyroscope.Profiler.Instance.SetAllocationTrackingEnabled(true);
+Pyroscope.Profiler.Instance.SetExceptionTrackingEnabled(true);
+Pyroscope.Profiler.Instance.SetContentionTrackingEnabled(true);
+Pyroscope.Profiler.Instance.SetCPUTrackingEnabled(true);
+Pyroscope.Profiler.Instance.SetExceptionTrackingEnabled(true);
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
